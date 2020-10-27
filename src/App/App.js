809b85306825e3ba83d5453bd68ec0b4399308a0
LiYuan1229 <823260355@qq.com>
2020-10-27 22:00:01 +0800

    [liyuan] finish add learner and show learner

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,74 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Learner from '../components/Learner';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      LearnerList: [],
+      addLearner: false,
+    };
+  }
+
+  componentDidMount() {
+    this.refreshLearnerList();
+  }
+
+  refreshLearnerList = () => {
+    this.fetchLearnerList().then((learnerList) => {
+      this.setState({ learnerList });
+    });
+  };
+
+  fetchLearnerList = () => {
+    return fetch('http://localhost:9698/learners').then((response) => {
+      return response.json();
+    });
+  };
+
+  fetchAddLearner = (data) => {
+    return fetch('http://localhost:9698/learners', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    }).then((response) => {
+      return response.status === 201 ? Promise.resolve() : Promise.reject();
+    });
+  };
+
+  handleAddLearnerEvent = () => {
+    this.setState({ addLearner: true });
+  };
+
+  handleInputKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      this.setState({ addLearner: false });
+      this.fetchAddLearner({ name: event.target.value }).then(() => {
+        this.refreshLearnerList();
+      });
+    }
+  };
+
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+           学员列表
+        <div className="learner_list">
+        {this.state.LearnerList.map((learner) => (
+            <Learner key={learner.id} id={learner.id} name={learner.learnerName} />
+          ))}
+          {this.state.addLearner ? (
+            // eslint-disable-next-line jsx-a11y/no-autofocus
+            <input autoFocus onKeyDown={this.handleInputKeyDown} />
+          ) : (
+            <button id="add_learner_btn" type="button" onClick={this.handleAddLearnerEvent}>
+              +添加学员
+            </button>
+          )}
+        </div> 
       </div>
+        
     );
   }
 }
